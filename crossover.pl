f14a05650bf3dd3e9ed1007646008d146a65df88
Vishnu Raghuraman <vishnu.raman@gmail.com>
2018-05-16 00:59:00 +0530

    Added multiple subroutines to extract random number of atoms from a cluster, and the complementary number of atoms from the other cluster.

--- crossover.pl
+++ crossover.pl
@@ -18,8 +18,8 @@ sub print_cluster {
 
 sub read_file {
 
-	my @firstcluster  = ();
-    my @secondcluster = ();
+	  my @a  = ();
+    my @b = ();
 
     my $fname = $_[0];
     open(my $fh, '<:encoding(UTF-8)', $fname)
@@ -28,7 +28,7 @@ sub read_file {
     while (my $row = <$fh>){
        chomp $row;
        my @atom = split(' ',$row);
-       push(@firstcluster, [@atom]);
+       push(@a, [@atom]);
     }
     
     $fname = $_[1];
@@ -38,115 +38,151 @@ sub read_file {
     while (my $row = <$fh>){
        chomp $row;
        my @atom = split(' ',$row);
-       push(@secondcluster, [@atom]);  
+       push(@b, [@atom]);  
     }
-    return (\@firstcluster, \@secondcluster);
+    return (\@a, \@b);
 }
 
 sub calculate_mean {
-   my @firstcluster = @{$_[0]};
-   my @secondcluster = @{$_[1]};
-   my @meanfirst  = (0, 0, 0);
-   my @meansecond = (0, 0, 0);
+   my @cluster = @{$_[0]};
+   my @mean = (0, 0, 0);
 
-   for(my $i = 0; $i <= $#firstcluster; $i++){
+   for(my $i = 0; $i <= $#cluster; $i++){
      for (my $j = 0; $j <= 2; $j++){
-        $meanfirst[$j] = $meanfirst[$j] + ($firstcluster[$i][$j + 1]/($#firstcluster + 1));
+        $mean[$j] = $mean[$j] + ($cluster[$i][$j + 1]/($#cluster + 1));
      }
    }
+   return \@mean;
+}
 
-   for(my $i = 0; $i <= $#secondcluster; $i++){
-     for(my $j = 0; $j <= 2; $j++){
-        $meansecond[$j] = $meansecond[$j] + ($secondcluster[$i][$j + 1]/($#secondcluster + 1));
-     }
-   }
-   return (\@meanfirst, \@meansecond)
+sub generate_random_numbers {
+  my @numberofeachtype = @{$_[0]};
+  my @randnumbers = ();
+  my @complementarynumbers = ();
+  my $tempnumber = 0;
+  for (my $i = 0; $i <= $#numberofeachtype; $i++){
+    $tempnumber = int(rand($numberofeachtype[$i] + 1));
+    push(@randnumbers, $tempnumber);
+    push(@complementarynumbers, ($numberofeachtype[$i] - $tempnumber))
+  }
+  return (\@randnumbers, \@complementarynumbers);
+}
+
+sub number_of_types {
+  my @types= ();
+  my @cluster = @{$_[0]};
+  push(@types, $cluster[0][0]);
+  for(my $i = 1; $i <= $#cluster; $i++){
+    chomp($cluster[$i][0]);
+    my $v = 'true';
+    for (my $j = ($i - 1); $j >= 0; $j--){
+            chomp($cluster[$j][0]);
+      if ($cluster[$i][0] eq $cluster[$j][0]){
+        $v = 'false';
+      }
+    }
+    if ($v eq 'true'){
+      push(@types, $cluster[$i][0]);
+    }
+  }
+  return \@types;
+}
+
+sub atoms_of_each_type {
+  my @types = @{$_[0]};
+  my @cluster = @{$_[1]};
+  my @numberofeachtype = ();
+  my $number = 0;
+
+  for (my $i = 0; $i <= $#types; $i++){
+    $number = 0;
+    for (my $j = 0; $j <= $#cluster; $j++){
+      if ($cluster[$j][0] eq $types[$i]){
+        $number = $number + 1;
+      }
+    }
+    push(@numberofeachtype, $number);
+  }
+  return \@numberofeachtype;
 }
 
 sub extract_atoms_of_type {
 	my @cluster = @{$_[0]};
 	my @types = @{$_[1]};
-	my $numbers = @{$_[2]};
+	my @numbers = @{$_[2]};
 	my $count = 0;
 	my @extractedcluster = ();
     
     for(my $j = 0; $j <= $#types; $j++){
+       $count = 0;
        for (my $i = 0; $i <= $#cluster; $i++){
-       	  if ($type eq $cluster[$i][0]){
+          if ($count >= $numbers[$j]){
+            last;
+          }
+       	  if ($types[$j] eq $cluster[$i][0]){
        	  	push(@extractedcluster, $cluster[$i]);
-       	  	$count = $count + 1
-       	  }
-       	  if ($count >= $numbers[j]){
-       	  	break;
+       	  	$count = $count + 1;
        	  }
        }
    }
+   return \@extractedcluster;
 }
 
-sub make_cut {
+sub shift_upwards {
+  my @cluster = @{$_[0]};
+  my $lowest = 0;
+
+  for (my $i = 0; $i <= $#cluster; $i++){
+    if ($cluster[$i][3] < $lowest){
+      $lowest = $cluster[$i][0];
+    }
+  }
+  for (my $i = 0; $i <= $#cluster; $i++){
+    $cluster[$i][3] = $cluster[$i][3] + $lowest;
+  }
+  return \@cluster;
+}
+
+sub shift_downwards {
+  my @cluster = @{$_[0]};
+  my $highest = 0;
+
+  for (my $i = 0; $i <= $#cluster; $i++){
+    if ($cluster[$i][3] > $highest){
+      $highest = $cluster[$i][0];
+    }
+  }
+  for (my $i = 0; $i <= $#cluster; $i++){
+    $cluster[$i][3] = $cluster[$i][3] - $highest;
+  }
+  return \@cluster;
+}
+
+sub shift_to_origin {
+  my @cluster = @{$_[0]};
+  my @mean = @{calculate_mean(\@cluster)};
+
+  for (my $i = 0; $i <= $#cluster; $i++){
+    $cluster[$i][1] = $cluster[$i][1] - $mean[1];
+    $cluster[$i][2] = $cluster[$i][2] - $mean[2];
+  }
+  return \@cluster;
+}
+
+sub join_cuts {
+  my @firstcut = @{$_[0]};
+  my @secondcut = @{$_[1]};
+  my @crossover = ();
   
-  my @firstcluster  = @{$_[0]};
-  my @secondcluster = @{$_[1]};
-  my $meanfirst  = $_[2];
-  my $meansecond = $_[3];    
-  my @firstcut   = ();
-  my @secondcut  = ();
-  
-  for(my $i = 0; $i <= $#firstcluster; $i++){
-     if ($firstcluster[$i][3] > $meanfirst){
-       push(@firstcut, $firstcluster[$i]);
-     }
+  for (my $i = 0; $i <= $#firstcut; $i++){
+    push(@crossover, $firstcut[$i]);
   }
 
-  for(my $i = 0; $i <= $#secondcluster; $i++){
-     if ($secondcluster[$i][3] < $meansecond){
-       push(@secondcut, $secondcluster[$i]);
-     }
-   }
-   return (\@firstcut, \@secondcut)
+  for (my $i = 0; $i <= $#secondcut; $i++){
+    push(@crossover, $secondcut[$i]);
+  }
+  return \@crossover;
 }
-
-sub number_of_types {
-	my @types= ();
-	my @cluster = @{$_[0]};
-	push(@types, $cluster[0][0]);
-	for(my $i = 1; $i <= $#cluster; $i++){
-		chomp($cluster[$i][0]);
-		my $v = 'true';
-		for (my $j = ($i - 1); $j >= 0; $j--){
-            chomp($cluster[$j][0]);
-			if ($cluster[$i][0] eq $cluster[$j][0]){
-				$v = 'false';
-			}
-		}
-		if ($v eq 'true'){
-			push(@types, $cluster[$i][0]);
-		}
-	}
-	return \@types;
-}
-
-sub atoms_of_type {
-	my @types = @{$_[0]};
-	my @cluster = @_{$_[1]};
-	my @numberofeachtype = ();
-
-	for (my $i = 0; $i <= $#types; $i++){
-		my $number = 0;
-		for (my $j = 0; $j ,= $#cluster, $j++){
-			if ($cluster[$j][0] eq $types[$i]){
-				$number = $number + 1;
-			}
-		}
-		push(@numberofeachtype, $number);
-	}
-	return \@numberofeachtype;
-}
-
-# Defining the arrays
-#-----------------------------------------------------------------------------------------------
-
-my @crossover = ();
 
 # Populating the arrays with file data
 #-----------------------------------------------------------------------------------------------
@@ -154,30 +190,49 @@ my @crossover = ();
 my ($firstclusterref, $secondclusterref) = read_file($ARGV[0], $ARGV[1]);
 my @firstcluster  = @{$firstclusterref};
 my @secondcluster = @{$secondclusterref};
+@firstcluster  = reverse sort { $a->[3] <=> $b->[3] } @firstcluster;
+@secondcluster = sort { $a->[3] <=> $b->[3] } @secondcluster;
 
 # Now let us calculate the central co-ordinates
 #-----------------------------------------------------------------------------------------------
 
-my ($firstmeanref, $secondmeanref) = calculate_mean(\@firstcluster, \@secondcluster);
-my @meanfirst = @{$firstmeanref};
-my @meansecond = @{$secondmeanref};
+my @meanfirst = @{calculate_mean(\@firstcluster)};
+my @meansecond = @{calculate_mean(\@secondcluster)};
 
 print "Centre Coordinates of First Cluster:\n";
 print "$meanfirst[0], ", "$meanfirst[1], ", "$meanfirst[2]\n";
 print "Centre Coordinates of Second Cluster:\n";
 print "$meansecond[0], ", "$meansecond[1], ", "$meansecond[2]\n\n";
 
-# Making cuts (We need to add all the conditions here)
+# Finding the types of atoms and no of each
 #-----------------------------------------------------------------------------------------------
 
-# 1. Need to first know how many types of atoms are there.
-# 2. Then the no of each type has to be calculated.
-
 my @types = @{number_of_types(\@firstcluster)};
+my @numberofeachtype = @{atoms_of_each_type(\@types, \@firstcluster)};
 
-my ($firstcutref, $secondcutref) = make_cut(\@firstcluster, \@secondcluster, $meanfirst[2], $meansecond[2]);
-my @firstbasecut  = @{$firstcutref};
-my @secondbasecut = @{$secondcutref};
+# Generating a random number of atoms of each type to extract from the cluster
+#-----------------------------------------------------------------------------------------------
+
+my ($randomnumbersref, $complementarynumbersref) = generate_random_numbers(\@numberofeachtype);
+my @randnumbers = @{$randomnumbersref};
+my @complementarynumbers = @{$complementarynumbersref}; 
+
+# Base First Cut (Taking out essential atoms) 
+#-----------------------------------------------------------------------------------------------
+
+my @basefirstcut = @{extract_atoms_of_type(\@firstcluster, \@types, \@randnumbers)};
+my @basesecondcut = @{extract_atoms_of_type(\@secondcluster, \@types, \@complementarynumbers)};
+
+# Shifting the cuts and joining them to form the final crossover result
+#-----------------------------------------------------------------------------------------------
+
+my @upwardshiftedfirstcut    = @{shift_upwards(\@basefirstcut)};
+my @downwardshiftedsecondcut = @{shift_downwards(\@basesecondcut)};
+
+my @finalfirstcut = @{shift_to_origin(\@upwardshiftedfirstcut)};
+my @finalsecondcut = @{shift_to_origin(\@downwardshiftedsecondcut)};
+
+my @crossover = @{join_cuts(\@finalfirstcut, \@finalsecondcut)};
 
 #-----------------------------------------------------------------------------------------------
 # Printing out cuts
@@ -193,36 +248,17 @@ my $secondclusterlength = $#secondcluster + 1;
 print "Length of Second Cluster: ", "$secondclusterlength\n\n";
 
 print "First Cut\n";
-print_cluster(\@firstcut);
-my $firstcutlength = $#firstcut + 1;
-print "Length of First Cut: ", "$firstcutlength\n\n";
+print_cluster(\@finalfirstcut);
+my $finalfirstcutlength = $#finalfirstcut + 1;
+print "Length of First Cut: ", "$finalfirstcutlength\n\n";
 
 print "Second Cut\n";
-print_cluster(\@secondcut);
-my $secondcutlength = $#secondcut + 1;
-print "Length of Second Cut: ", "$secondcutlength\n\n";
+print_cluster(\@finalsecondcut);
+my $finalsecondcutlength = $#finalsecondcut + 1;
+print "Length of Second Cut: ", "$finalsecondcutlength\n\n";
 
-# Shifting the cuts
-#-----------------------------------------------------------------------------------------------
-
-for(my $i = 0; $i <= $#firstcut; $i++){
-   for(my $j = 1; $j <= 3; $j++){
-      $firstcut[$i][$j] = $firstcut[$i][$j] - $meanfirst[$j - 1];
-   }
-   push(@crossover, $firstcut[$i]);
-}
-
-for(my $i = 0; $i <= $#secondcut; $i++){
-   for(my $j = 1; $j <= 3; $j++){
-      $secondcut[$i][$j] = $secondcut[$i][$j] - $meansecond[$j - 1];
-   }
-   push(@crossover, $secondcut[$i]);
-}
-
+print "Final Crossover\n";
+print_cluster(\@crossover);
 my $crossoverlength = $#crossover + 1;
-print "Length of Crossover, ", "$crossoverlength\n";
-print "Final Crossover Result\n";
-for(my $i = 0; $i <= $#crossover; $i++){
-    print "atom ","$crossover[$i][1] ","$crossover[$i][2] ","$crossover[$i][3] ","$crossover[$i][0]\n";
-}
+print "Length of Crossover: ", "$crossoverlength\n";
 #-----------------------------------------------------------------------------------------------
