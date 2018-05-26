0968d420fab2d9f8f18fc222e6568488c2a9bade
Vishnu Raghuraman <vishnu.raman@gmail.com>
2018-05-26 11:17:24 +0530

    Working on separating cluster from the substrate

--- substrate_crossover.pl
+++ substrate_crossover.pl
@@ -48,16 +48,67 @@ sub get_magnitude_of_lattice_vectors {
 	return \@magnitude;
 }
 
-sub find_random_distance_along_a {
+sub check_along_a {
 	my @atom = @{$_[0]};
 	my @collection = @{$_[1]};
-	my $distance = 0.0;
-	for (my $i = 0; $i <= $#collection; $i++){
-		if ($atom[2] == $collection[$i][2] and $atom[3] == $collection[$i][3]){
-			$distance = $collection[$i][1] - $atom[1];
-			return $distance,
+	my $check = 'true';
+	for (my $j = 0; $j <= $#collection; $j++){
+		if ($atom[2] == $collection[$j][2] and $atom[3] == $collection[$j][3]
+			and $atom[1] != $collection[$j][1]){
+			$check = 'false';
 		}
 	}
+	return $check;
+}
+
+sub check_along_b {
+	my @atom = @{$_[0]};
+	my @collection = @{$_[1]};
+	my $check = 'true';
+	for (my $j = 0; $j <= $#collection; $j++){
+		if ($atom[1] == $collection[$j][1] and $atom[3] == $collection[$j][3]
+			and $atom[2] != $collection[$j][2]){
+			$check = 'false';
+		}
+	}
+	return $check;
+}
+
+sub check_along_c {
+	my @atom = @{$_[0]};
+	my @collection = @{$_[1]};
+	my $check = 'true';
+	for (my $j = 0; $j <= $#collection; $j++){
+		if ($atom[1] == $collection[$j][1] and $atom[2] == $collection[$j][2]
+			and $atom[3] != $collection[$j][3]){
+			$check = 'false';
+		}
+	}
+	return $check;
+}
+
+sub separate_cluster_from_substrate {
+	my @collection = @{$_[0]};
+	my @cluster = ();
+	my $check_a = 0;
+	my $check_b = 0;
+	my $check_c = 0;
+	my @atom = (0, 0, 0, 0, 0);
+
+	for (my $i = 0; $i <= $#collection; $i++){
+		$atom[0] = $collection[$i][0];
+		$atom[1] = $collection[$i][1];
+		$atom[2] = $collection[$i][2];
+		$atom[3] = $collection[$i][3];
+		$atom[4] = $collection[$i][4];
+		$check_a = check_along_a(\@atom, \@collection);
+		$check_b = check_along_b(\@atom, \@collection);
+		$check_c = check_along_c(\@atom, \@collection);
+		if ($check_a eq 'true' and $check_b eq 'true' and $check_c eq 'true'){
+			push(@cluster, $collection[$i]);
+		}
+	}
+	return \@cluster;
 }
 
 sub convert_frac_to_atom {
@@ -83,12 +134,11 @@ my @lattice_vectors = @{$lattice_vectors_ref};
 @collection = @{$collection_ref};
 
 my @magnitude = @{get_magnitude_of_lattice_vectors(\@lattice_vectors)};
-@collection = @{convert_frac_to_atom(\@collection, \@lattice_vectors)};
 
-for (my $i = 0; $i <= $#lattice_vectors; $i++){
-	print "$lattice_vectors[$i][0] ","$lattice_vectors[$i][1] ","$lattice_vectors[$i][2] ","$lattice_vectors[$i][3]\n";
-}
+my @cluster = @{separate_cluster_from_substrate(\@collection)};
 
-for (my $i = 0; $i <= $#collection; $i++){
-	print "atom ","$collection[$i][1] ","$collection[$i][2] ","$collection[$i][3] ","$collection[$i][4]\n";
+for (my $i = 0; $i <= $#cluster; $i++){
+	print "$cluster[$i][0] ","$cluster[$i][1] ","$cluster[$i][2] ","$cluster[$i][3] ","$cluster[$i][4]\n"; 
 }
+print "$#cluster\n";
+print "$#collection\n";
